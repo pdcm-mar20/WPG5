e84f9091488a594750a9d82b1b7cb535efd4d07d
RobyRAX <robyalexander571@gmail.com>
2021-11-11 07:50:22 +0800

    SistemItem_Baru

---
+++ Assets/Scripts/PlayerItemActivation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemActivation : MonoBehaviour
+{
+    private bool isPlayerHasItem;
+
+    private void Update()
+    {
+        ItemCheck();
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(DataItems.shoe) //Item Shoe
+            {
+                StartCoroutine("SpeedActive");
+            }
+
+            else if(DataItems.oil)
+            {
+                StartCoroutine("OilOn");
+            }
+        }
+    }
+
+    void ItemCheck()
+    {
+        if (DataItems.oil || DataItems.obstacle || DataItems.shield || DataItems.shoe || DataItems.waterBall > 0)
+        {
+            isPlayerHasItem = true;
+            //Debug.Log("player got item");
+        }
+        else
+        {
+            isPlayerHasItem = false;
+            //Debug.Log("player doesn't got item");
+        }    
+
+        if(DataItems.shoe)
+        {
+            Debug.Log("Sepatu_Dapet");
+        }
+        else if(DataItems.oil)
+        {
+            Debug.Log("Oli_Dapet");
+        }
+        
+        if(GameObject.Find("CurrentItem") != null)
+        {
+            //Debug.Log("Ada");
+        }
+    }
+
+    public IEnumerator SpeedActive()
+    {
+        PlayerMovement.speed.Value = 10;
+        yield return new WaitForSeconds(5);
+        PlayerMovement.speed.Value = 5;
+        DataItems.shoe = false;
+    }
+
+    public IEnumerator OilOn()
+    {
+        PlayerMovement.speed.Value = 3;
+        yield return new WaitForSeconds(5);
+        DataItems.oil = false;
+        PlayerMovement.speed.Value = 5;
+    }
+}
