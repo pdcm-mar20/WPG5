7c1d49d45513eb79dcfb80149c76043ac34cb497
imam <imambiladi27@gmail.com>
2021-11-13 09:29:00 +0700

    Tambahan Script FInish Dan asset FInish

---
+++ Assets/Scripts/OnTrigger_Scene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTrigger_Scene : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+    //  public GameObject Endscrn;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(index);
+            SceneManager.LoadScene(levelName);
+            //Endscrn.gameObject.SetActive(true);
+
+        }
+    }
+
+}
